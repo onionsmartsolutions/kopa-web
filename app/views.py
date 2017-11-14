52eb2ea52874b616ac5d6f508d2b3ad8a90d52f1
Gray Jumba <gjumba@onion.co.ke>
2017-11-14 20:10:59 +0300

    Changed API rules

--- app/views.py
+++ app/views.py
@@ -73,9 +73,9 @@ class StatementList(generics.ListCreateAPIView):
         by filtering against a `username` query parameter in the URL.
         """
         queryset = Statement.objects.all()
-        user = self.request.query_params.get('user', None)
+        user = self.request.query_params.get('user_id', None)
         if user is not None:
-            queryset = queryset.filter(user=user)
+            queryset = queryset.filter(user_id=user)
         return queryset
 
 
